4ddb9f5be428bccf23a8732ae1658d163318e3aa
Daven Wu <epikglow@gmail.com>
2014-10-12 04:23:50 -0400

    Added friend picker for real

--- app/src/main/java/hackru2014f/friendcast/FriendPickerActivity.java
+++ app/src/main/java/hackru2014f/friendcast/FriendPickerActivity.java
@@ -67,13 +67,6 @@ public class FriendPickerActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.friend_picker, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
